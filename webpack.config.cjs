257965133f59c4c1f62697781607bbe9e64390c2
Kavithra2002 <kmethnula@gmail.com>
2026-03-10 12:11:49 +0530

    vervel error

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-  mode: "development",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
